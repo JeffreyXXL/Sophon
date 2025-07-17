4a36c836c35d0dc5b168c6fcb312d7233076a123
Jeffrey <xiajie727@vip.qq.com>
2025-07-17 10:35:13 +0800

    feat: 增加autofac容器注册扩展方法与注册接口

---
+++ Common/DI/IModuleRegister.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DI
+{
+    public interface IModuleRegister
+    {
+        /// <summary>
+        /// 注册模组内所有需要注册的类
+        /// </summary>
+        /// <param name="builder"></param>
+        void Register(ContainerBuilder builder);
+    }
+}
---
+++ Common/DI/RegisterAllModule.cs
@@ -0,0 +1,68 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DI
+{
+    public static class RegisterAllModule
+    {
+        /// <summary>
+        /// 注册所有模组的扩展方法
+        /// </summary>
+        /// <param name="builder"></param>
+        public static ContainerBuilder RegisterAllModuleExt(this ContainerBuilder builder)
+        {
+            //1、加载所有assembly
+            List<Assembly> assemblies = new List<Assembly>();
+            foreach (var name in assemblyNames)
+            {
+                try
+                {
+                    assemblies.Add(Assembly.Load(name));
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine($"加载{name}失败：" + e.Message);
+                }
+            }
+            //2、获取所有继承IModuleRegister的类
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes().Where(x => typeof(IModuleRegister).IsAssignableFrom(x)
+                                              && !x.IsInterface);
+                //3、使用所有类的RegisterModule()方法
+                foreach (var type in types)
+                {
+                    try
+                    {
+                        var instance = (IModuleRegister)Activator.CreateInstance(type);
+                        instance.Register(builder);
+                    }
+                    catch (Exception e)
+                    {
+                        Trace.WriteLine(e.Message);
+                    }
+                }
+            }
+            return builder;
+        }
+
+        /// <summary>
+        /// 解决方案内所有项目名称集合
+        /// </summary>
+        private static readonly List<string> assemblyNames = new List<string>
+        {
+            "Common",
+            "Sophon.Application",
+            "Sophon.Core",
+            "Sophon.Infrastructure",
+            "Sophon.UI"
+        };
+    }
+}
