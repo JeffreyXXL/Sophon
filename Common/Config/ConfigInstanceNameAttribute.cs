75e16196897d5838d14b23ac2a028f1bfac54bdb
Jeffrey <xiajie727@vip.qq.com>
2025-07-21 16:27:30 +0800

    feat: 增加ini实现，INI支持集合操作，增加特性用于ini的section，增加ini自定义异常

---
+++ Common/Config/ConfigInstanceNameAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// 如果使用ini配置，则类需要使用此特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IniConfigInstanceNameAttribute : Attribute
+    {
+        public IniConfigInstanceNameAttribute(string instanceName)
+        {
+
+        }
+    }
+}
---
+++ Common/Config/IniConfigException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class IniConfigException : Exception
+    {
+        public IniConfigException(string message) : base(message) { }
+    }
+}
