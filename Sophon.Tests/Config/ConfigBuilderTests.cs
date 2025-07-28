cf87d1a021dbd434ae56725807d677af3be27e92
Jeffrey <xiajie727@vip.qq.com>
2025-07-28 14:54:58 +0800

    test: 配置文件相关类测试

---
+++ Sophon.Tests/Config/ConfigBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Tests.Config
+{
+    public class DummyConfig
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+    }
+
+    [TestFixture]
+    public class ConfigBuilderTests
+    {
+        /// <summary>
+        /// 测试是否可以正常build
+        /// </summary>
+        [Test]
+        public void BuildConfig_Test()
+        {
+            var builder = new ConfigBuilder();
+            builder.SetValue("Name", "MotorX")
+                   .SetValue("Speed", 120);
+
+            var config = builder.BuildConfig<DummyConfig>();
+            Assert.That(config is DummyConfig);
+            Assert.That(config.Name, Is.EqualTo("MotorX"));
+            Assert.That(config.Speed, Is.EqualTo(120));
+        }
+    }
+}
---
+++ Sophon.Tests/Config/Serializer/XmlConfigSerializerTest.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Tests.Config.Serializer
+{
+    [TestFixture]
+    public class XmlConfigSerializerTest
+    {
+        private XmlConfigSerializer _serializer = new XmlConfigSerializer();
+
+        /// <summary>
+        /// 测试是否可以正常序列化/反序列化
+        /// </summary>
+        [Test]
+        public void Serialize_Deserialize_Test()
+        {
+            var step = new StepTestClass() { Name = "MotorMove", Delay = 10, Enabled = true };
+
+            var jsoncontent = _serializer.Serialize(step);
+            var deserialized = _serializer.Deserialize<StepTestClass>(jsoncontent);
+
+            Assert.That(deserialized.Name == "MotorMove");
+            Assert.That(deserialized.Delay == 10);
+            Assert.That(deserialized.Enabled == true);
+        }
+
+        /// <summary>
+        /// 测试是否可以正常抛出异常
+        /// </summary>
+        [Test]
+        public void Serialize_Deserialize_Throw_Test()
+        {
+            Assert.Throws<ConfigDeserializeException>(() => _serializer.Deserialize<StepTestClass>("123123"));
+        }
+    }
+}
