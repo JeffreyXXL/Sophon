c27c1f95034aaa8352373b1b19819ab6250172e3
Jeffrey <xiajie727@vip.qq.com>
2025-07-29 16:59:42 +0800

    feat：增加流程单步接口和单步结果类

---
+++ Sophon.Core/Flow/IFlowStep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sophon.Core
+{
+    /// <summary>
+    /// 流程单步接口
+    /// </summary>
+    public interface IFlowStep
+    {
+        int StepName { get; }
+
+        Task<StepResult> AsyncExcuteStep(IFlowContext context);
+    }
+
+
+}
---
+++ Sophon.Core/Flow/StepResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Core
+{
+    public class StepResult
+    {
+        public StepStatus Status { get; set; }
+        public string message { get; set; }
+
+
+        public static StepResult Success()
+        {
+            return new StepResult
+            {
+                Status = StepStatus.Success
+            };
+        }
+
+        public static StepResult Failure(string msg)
+        {
+            return new StepResult()
+            {
+                Status = StepStatus.Failure,
+                message = msg
+            };
+        }
+    }
+
+    public enum StepStatus
+    {
+        Success,
+        Failure,
+        Timeout,
+        Cancelled,
+        Skipped
+    }
+}
