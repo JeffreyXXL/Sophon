4c1791dbeafa106c1f14add4d94490dbd69b30d9
Jeffrey <xiajie727@vip.qq.com>
2025-08-04 16:32:15 +0800

    wip： 增加workstation，里面内容待完善

---
+++ Sophon.Core/WorkStation/WorkStation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Core
+{
+    public class WorkStation : IWorkStation
+    {
+        #region 构造函数
+        public WorkStation(string workStationName, IFlowEngineFactory flowEngineFactory, IFlowContextFactory flowContextFactory)
+        {
+            _flowEngineFactory = flowEngineFactory;
+            _flowcontextfactory = flowContextFactory;
+        }
+        #endregion
+
+        #region 属性
+
+        #endregion
+        private readonly IFlowEngineFactory _flowEngineFactory;
+        private readonly IFlowContextFactory _flowcontextfactory;
+        #region 字段
+
+
+        #endregion
+
+        #region 方法
+
+        #endregion
+    }
+}
---
+++ Sophon.Core/WorkStation/WorkStationFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Core
+{
+    public class WorkStationFactory : IWorkStationFactory
+    {
+        private readonly ConcurrentDictionary<string, IWorkStation> _workStationcache = new ConcurrentDictionary<string, IWorkStation>();
+
+        private IFlowEngineFactory _flowEngineFactory;
+        private IFlowContextFactory _flowcontextfactory;
+
+        public WorkStationFactory(IFlowEngineFactory flowEngineFactory, IFlowContextFactory flowContextFactory)
+        {
+            _flowEngineFactory = flowEngineFactory;
+            _flowcontextfactory = flowContextFactory;
+        }
+
+        public IWorkStation CreateWorkStation(string workStationName)
+        {
+            return _workStationcache.GetOrAdd(workStationName, new WorkStation(workStationName, _flowEngineFactory, _flowcontextfactory));
+        }
+    }
+}
