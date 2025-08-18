854d81a5461e18ef5a779461b7c47313b39afe92
Jeffrey <xiajie727@vip.qq.com>
2025-08-18 11:53:44 +0800

    wip:轴卡部分接口新建

---
+++ Sophon.Infrastructure/Hardware/Interface/IAxisController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Infrastructure
+{
+    public interface IAxisController
+    {
+        bool ServeOn(int cardNo, int axisNo);
+
+        bool ServeOff(int cardNo, int axisNo);
+
+        bool Home(int axisNo);
+
+        bool MoveAbs(int axisNo, double position);
+
+        bool MoveRel(int axisNo, double distance);
+
+        double GetPos(int axisNo);
+
+        double GetTorque(int axisNo);
+    }
+}
---
+++ Sophon.Infrastructure/Hardware/Interface/IIOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Infrastructure
+{
+    public interface IIOController
+    {
+        void SetOut(int cardNo, int ioNo, bool state);
+
+        bool ReadIn(int cardNo, int ioNo);
+
+        bool ReadOut(int cardNo, int ioNo);
+    }
+}
