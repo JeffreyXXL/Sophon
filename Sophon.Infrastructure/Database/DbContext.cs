3f870b44b4db8c6b9bf5eb282ce9ad090b080f87
JeffreyXXL <xiajie727@vip.qq.com>
2025-08-25 19:04:54 +0800

    wip ： 增加dbcontext

--- Sophon.Infrastructure/Database/DbContext.cs
+++ Sophon.Infrastructure/Database/DbContext.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
 
 namespace Sophon.Infrastructure
 {
     public class DbContext
     {
+        public static SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+        {
+            DbType = DbType.Sqlite,
+            ConnectionString = "Data Source =" + Path.Combine(ConfigurationManager.AppSettings["DatabasePath"], "SophonData.db") + ";",
+            IsAutoCloseConnection = true,
+            InitKeyType = InitKeyType.Attribute
+        });
     }
 }
