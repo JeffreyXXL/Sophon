c5270db69072543092d46afbc186ffbd018f7fe8
Jeffrey <xiajie727@vip.qq.com>
2026-01-12 20:23:46 +0800

    wip: 增加板卡工厂

---
+++ Sophon.Infrastructure/Hardware/Drives/MotionCardFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophon.Infrastructure
+{
+    public class MotionCardFactory : IMotionCardFactory
+    {
+        public IAxisController CreateAxisController()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IIOController CreateIOController()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
